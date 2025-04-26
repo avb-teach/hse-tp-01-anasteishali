7536cf6b73a2420150a35a55029a0f94d1caaaac
anasteishali <aachekulaeva@edu.hse.ru>
2025-04-26 14:48:06 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,55 +1,63 @@
 #!/bin/bash
 
-if [[ "$1" == "--max_depth" ]]; then
-    MAX_DEPTH="$2"
-    INPUT_DIR="$3"
-    OUTPUT_DIR="$4"
-else
-    INPUT_DIR="$1"
-    OUTPUT_DIR="$2"
-    MAX_DEPTH=""
-fi
+max_depth=""
+input_dir=""
+output_dir=""
 
-if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
-    echo "Usage: $0 [--max_depth N] <input_dir> <output_dir>"
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --max_depth)
+            max_depth="$2"
+            shift 2
+            ;;
+        *)
+            if [ -z "$input_dir" ]; then
+                input_dir="$1"
+            elif [ -z "$output_dir" ]; then
+                output_dir="$1"
+            else
+                echo "Неизвестный параметр: $1"
+                exit 1
+            fi
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
+    echo "Использование: $0 [--max_depth N] <входная_директория> <выходная_директория>"
     exit 1
 fi
 
-mkdir -p "$OUTPUT_DIR"
+if [ ! -d "$input_dir" ]; then
+    echo "Ошибка: входная директория не существует"
+    exit 1
+fi
 
-declare -A seen
+mkdir -p "$output_dir"
 
-copy_files() {
-    local input_dir="$1"
-    local output_dir="$2"
-    local max_depth="$3"
-
-    local base_depth=$(tr -dc '/' <<< "$input_dir" | wc -c)
-
-    find "$input_dir" -type f | while read -r filepath; do
-        local file_depth=$(tr -dc '/' <<< "$filepath" | wc -c)
-        local relative_depth=$((file_depth - base_depth))
-
-        if [[ -n "$max_depth" && "$relative_depth" -gt "$max_depth" ]]; then
-            continue
-        fi
-
-        local filename=$(basename "$filepath")
-        local target="$output_dir/$filename"
-
-        if [[ -e "$target" ]]; then
-            counter=${seen["$filename"]}
-            counter=$((counter + 1))
-            seen["$filename"]=$counter
-            extension="${filename##*.}"
-            name="${filename%.*}"
-            target="$output_dir/${name}_${counter}.$extension"
-        else
-            seen["$filename"]=0
-        fi
-
-        cp "$filepath" "$target"
+copy_with_suffix() {
+    local src="$1"
+    local dest_dir="$2"
+    local filename=$(basename "$src")
+    local base="${filename%.*}"
+    local ext="${filename##*.}"
+    local counter=1
+    local dest="$dest_dir/$filename"
+    
+    while [ -e "$dest" ]; do
+        dest="$dest_dir/${base}_$counter.$ext"
+        ((counter++))
     done
+    
+    cp "$src" "$dest"
 }
 
-copy_files "$INPUT_DIR" "$OUTPUT_DIR" "$MAX_DEPTH"
+export -f copy_with_suffix
+
+find_cmd="find \"$input_dir\" -type f"
+if [ -n "$max_depth" ]; then
+    find_cmd="$find_cmd -maxdepth $max_depth"
+fi
+
+eval "$find_cmd" -exec bash -c 'copy_with_suffix "$0" "$1"' {} "$output_dir" \;
