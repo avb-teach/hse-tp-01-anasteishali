89fef3c84fd693d7729d72faf42882f4fdb1d72d
anasteishali <aachekulaeva@edu.hse.ru>
2025-04-26 14:54:28 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
-input_dir="$1"
-output_dir="$2"
-max_depth="$3"
-
-
-if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
-  echo "Usage: $0 <input_dir> <output_dir> [max_depth]"
-  exit 1
-fi
-
-
-mkdir -p "$output_dir"
-
-
-if [ -z "$max_depth" ]; then
-  files=$(find "$input_dir" -type f)
+if [[ "$1" == "--max_depth" ]]; then
+    MAX_DEPTH="$2"
+    INPUT_DIR="$3"
+    OUTPUT_DIR="$4"
 else
-  files=$(find "$input_dir" -maxdepth "$max_depth" -type f)
+    INPUT_DIR="$1"
+    OUTPUT_DIR="$2"
+    MAX_DEPTH=""
 fi
 
+if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
+    echo "Usage: $0 [--max_depth N] <input_dir> <output_dir>"
+    exit 1
+fi
 
-for filepath in $files; do
-  filename=$(basename "$filepath")
-  target="$output_dir/$filename"
+mkdir -p "$OUTPUT_DIR"
 
-  counter=1
-  while [ -e "$target" ]; do
-    target="$output_dir/${filename%.*}$counter.${filename##*.}"
-    counter=$((counter + 1))
-  done
+declare -A seen
 
-  cp "$filepath" "$target"
-done
+copy_files() {
+    local input_dir="$1"
+    local output_dir="$2"
+    local max_depth="$3"
+
+    find "$input_dir" -type f | while read -r filepath; do
+        relative_path="${filepath#$input_dir/}"  
+
+      
+        if [[ "$relative_path" == *"/"* ]]; then
+            depth=$(grep -o "/" <<< "$relative_path" | wc -l)
+        else
+            depth=0
+        fi
+
+       
+        if [[ -n "$max_depth" && "$depth" -ge "$max_depth" ]]; then
+            continue
+        fi
+
+        filename=$(basename "$filepath")
+        target="$output_dir/$filename"
+
+        if [[ -e "$target" ]]; then
+            counter=${seen["$filename"]}
+            counter=$((counter + 1))
+            seen["$filename"]=$counter
+            extension="${filename##*.}"
+            name="${filename%.*}"
+            target="$output_dir/${name}_${counter}.$extension"
+        else
+            seen["$filename"]=0
+        fi
+
+        cp "$filepath" "$target"
+    done
+}
+
+copy_files "$INPUT_DIR" "$OUTPUT_DIR" "$MAX_DEPTH"
